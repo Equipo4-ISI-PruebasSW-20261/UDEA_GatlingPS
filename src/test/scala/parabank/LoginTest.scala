7e2dbbd42ac3648305e977d343d0ec3b46e4aae5
christian-gomezp <christian.gomezp@udea.edu.co>
2026-04-28 21:11:44 -0500

    Enhance LoginTest with load scenarios and assertions
    Added load testing scenarios for normal and peak traffic.

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -13,15 +13,39 @@ class LoginTest extends Simulation{
     .check(status.is(200))
 
   // 2 Scenario Definition
-  val scn = scenario("Login").
+  val scn100 = scenario("Login").
     exec(http("login")
       .get(s"/login/$username/$password")
        //Recibir información de la cuenta
       .check(status.is(200))
     )
 
+  val scn200 = scenario("Login").
+    exec(http("login")
+      .get(s"/login/$username/$password")
+       //Recibir información de la cuenta
+      .check(status.is(200))
+    )
+
+  val cargaNormal = scn100.inject(
+    rampUsers(100).during(10.seconds)
+  )
+
+  val cargaPico = scn200.inject(
+    rampUsers(200).during(20.seconds)
+  )
+
   // 3 Load Scenario
   setUp(
-    scn.inject(rampUsersPerSec(5).to(15).during(30))
-  ).protocols(httpConf);
+    cargaNormal,
+    cargaPico
+  ).protocols(httpConf)
+   .assertions(
+      // Tiempo medio ≤ 2s bajo carga normal
+      global.responseTime.mean.lte(2000),
+      // Tiempo máx ≤ 5s bajo carga pico
+      global.responseTime.max.lte(5000),
+      // Menos del 1% de errores
+      global.failedRequests.percent.lte(1)
+   )
 }
