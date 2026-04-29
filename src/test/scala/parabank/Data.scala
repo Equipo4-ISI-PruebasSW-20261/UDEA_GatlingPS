382ac9d6c4669e489d4558aa41482dfa26da8ec9
christian-gomezp <christian.gomezp@udea.edu.co>
2026-04-28 20:33:05 -0500

    Rename account ID and amount variables in Data object

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,8 +4,8 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "chrisg"
     val password = "chrisg"
-    val fromAccountId = "15120"
-    val toAccountId = "15231"
-    val amount = "1"
+    val fromAccountId_ = "15120"
+    val toAccountId_ = "15231"
+    val amount_ = "1"
 
 }
