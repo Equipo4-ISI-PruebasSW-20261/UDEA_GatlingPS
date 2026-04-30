f2d2e60f36519e86ee101dacb20c4f83e6ce731f
juan.duque31 <129987078+juanduque31@users.noreply.github.com>
2026-04-29 23:04:52 -0500

    Update LoginTest.scala

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -33,9 +33,12 @@ class LoginTest extends Simulation {
   // 3 Load Scenario
   // Ejecutar secuencialmente: primero carga normal, luego carga pico
   setUp(
-    scnNormal.inject(constantConcurrentUsers(100).during(30.seconds))
-    .andThen(
-      scnPico.inject(constantConcurrentUsers(200).during(30.seconds))
+    scnNormal.inject(
+      constantConcurrentUsers(100).during(30.seconds)
+    ),
+    scnPico.inject(
+      nothingFor(30.seconds),
+      constantConcurrentUsers(200).during(30.seconds)
     )
   ).protocols(httpConf)
    .assertions(
