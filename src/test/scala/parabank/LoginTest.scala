4fed20ff3ae54ab42692a1b13788f17590b4c559
juan.duque31 <129987078+juanduque31@users.noreply.github.com>
2026-04-29 22:10:55 -0500

    Update LoginTest.scala

--- src/test/scala/parabank/LoginTest.scala
+++ src/test/scala/parabank/LoginTest.scala
@@ -31,17 +31,17 @@ class LoginTest extends Simulation {
   // 3 Load Scenario
   // Ejecutar secuencialmente: primero carga normal, luego carga pico
   setUp(
-    scnNormal.inject(rampUsers(100).during(30.seconds)),
+    scnNormal.inject(constantConcurrentUsers(100).during(30.seconds)),
     scnPico.inject(
-      nothingFor(60.seconds),          // esperar que carga normal termine
-      rampUsers(200).during(30.seconds) // carga pico después
+      nothingFor(40.seconds),          // esperar que carga normal termine
+      constantConcurrentUsers(200).during(30.seconds) // carga pico después
     )
   ).protocols(httpConf)
    .assertions(
      // Historia 1: ≤ 2 segundos con 100 usuarios concurrentes (carga normal)
-     details("Login Carga Normal").responseTime.max.lte(2000),
+     details("Login Carga Normal").responseTime.percentile3.lte(2000),
      // Historia 1: ≤ 5 segundos con 200 usuarios concurrentes (carga pico)
-     details("Login Carga Pico").responseTime.max.lte(5000),
+     details("Login Carga Pico").responseTime.percentile3.lte(5000),
      // Tasa de error global ≤ 1%
      global.failedRequests.percent.lte(1)
    )
